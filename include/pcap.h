95300270702f2505c6d706c0bd0bcadbe0b29c2e
DeakinSimpson <deakinsimpson976@gmail.com>
2026-08-13 21:58:26 +1000

    added basic header reading

---
+++ include/pcap.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <cstdint>
+#include <fstream>
+
+/* ensures there is no padding added to the struct, every header read is the 
+   the exact correct bit lenth*/
+#pragma pack(push, 1)
+struct PcapHeader
+{
+    uint32_t magicNumber;
+    uint16_t majorVersion;
+    uint16_t minorVersion;
+    uint32_t reserved1;
+    uint32_t reserved2;
+    uint32_t snapLen;
+    uint32_t network;
+};
+#pragma pack(pop)
+
+class PcapFile
+{
+public:
+    PcapFile(std::ifstream& fs) { ReadPcapHeader(fs, this->header); }
+
+    inline PcapHeader getPcapHeader() { return this->header; }
+    bool isMicroSeconds() { return (this->header.magicNumber == 2712847316); }
+
+private:
+    PcapHeader header {};
+
+    static void ReadPcapHeader(std::ifstream& fs, PcapHeader& header)
+    {
+        // reinterperet cast is a bit level cast
+        fs.read(reinterpret_cast<char*>(&header), sizeof(header));
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <fstream> // for ifstream file reading
+#include <string>
+#include "pcap.h"
+
+#include <filesystem>
+
+namespace testVals
+{
+    const std::string fileLocation { "../test/pcap/01-test.pcap" };
+} 
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    // open pcap
+    std::ifstream fs;
+    fs.open(testVals::fileLocation, std::ifstream::binary);
+    if (!fs.good()) { return 1; }
+
+    PcapFile pcapFile { PcapFile(fs) };
 
     return 0;
 }
